beeef18004b0c3aba009bc43c72b0d5e82f0f5e7
geraldman <g3rald.dj@gmail.com>
2025-06-18 16:02:16 +0700

    about us view file

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <link rel="stylesheet" href="{{asset('css/about.css')}}">
+    </head>
+    <body>
+        
+    </body>
+</html>
